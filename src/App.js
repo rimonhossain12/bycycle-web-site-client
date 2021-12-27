f3122572376825761ae9a761e1d7d84a0a91150e
rimonspi12 <rimonmpi@gmail.com>
2021-12-27 23:12:22 +0600

    some file delete

--- src/App.js
+++ src/App.js
@@ -8,34 +8,35 @@ import Login from './Pages/Login/Login/Login/Login';
 import Register from './Pages/Login/Register/Register';
 
 
+
 function App() {
   return (
     <div className="App">
-      <Router>
-        <Switch>
-          <Route exact path="/">
-            <Home />
-          </Route>
-          <Route path="/home">
-            <Home />
-          </Route>
-          <Route path="/about">
-            <About />
-          </Route>
-          <Route path="/addProducts">
-            <AddProduct />
-          </Route>
-          <Route path="/loadAllProducts">
-            <AllProducts />
-          </Route>
-          <Route path="/login">
-            <Login/>
-          </Route>
-          <Route path="/register">
-            <Register/>
-          </Route>
-        </Switch>
-      </Router>
+        <Router>
+          <Switch>
+            <Route exact path="/">
+              <Home />
+            </Route>
+            <Route path="/home">
+              <Home />
+            </Route>
+            <Route path="/about">
+              <About />
+            </Route>
+            <Route path="/addProducts">
+              <AddProduct />
+            </Route>
+            <Route path="/loadAllProducts">
+              <AllProducts />
+            </Route>
+            <Route path="/login">
+              <Login />
+            </Route>
+            <Route path="/register">
+              <Register />
+            </Route>
+          </Switch>
+        </Router>
     </div>
   );
 }
