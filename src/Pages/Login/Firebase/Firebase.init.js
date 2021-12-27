f7780b00949036e3b074d1a130b092f974dc3e19
rimonspi12 <rimonmpi@gmail.com>
2021-12-27 21:45:33 +0600

    firebase initailiaze

---
+++ src/Pages/Login/Firebase/Firebase.init.js
@@ -0,0 +1,7 @@
+import { initializeApp } from "firebase/app";
+import firebaseConfig from "./Firebase.config";
+
+const initialAuthentication = () => {
+    initializeApp(firebaseConfig);
+}
+export default initialAuthentication;
---
+++ src/hooks/useFirebase.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const useFirebase = () => {
+    
+    return {
+
+    }
+};
+
+export default useFirebase;
