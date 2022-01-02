6aba9badfd1fef4daca8d199e778f1c93f681db7
rimonspi12 <rimonmpi@gmail.com>
2022-01-02 11:27:11 +0600

    review page design

--- src/Pages/DashBoard/Review/Review.js
+++ src/Pages/DashBoard/Review/Review.js
@@ -1,19 +1,48 @@
-import React from 'react';
-import { useForm } from "react-hook-form";
+import React, { useState } from 'react';
+import { Col, Row } from 'react-bootstrap';
+import ReactStars from "react-rating-stars-component";
+import useAuth from '../../../hooks/useAuth';
+
 import './Review.css';
 
 const Review = () => {
-    const { register, handleSubmit } = useForm();
-    const onSubmit = data => console.log(data);
+    const {user} = useAuth();
+    const [reviewCount, setReviewCount] = useState(0);
+    const ratingChanged = (newRating) => {
+        setReviewCount(newRating);
+        console.log(newRating);
+    };
+
     return (
         <div className='review-main'>
             <div className='review-div'>
-                <h2 className='fw-bold fs-4 text-warning mb-4'>Give Us Feedback</h2>
-                <form onSubmit={handleSubmit(onSubmit)}>
-                    <input {...register("firstName", { required: true, maxLength: 20 })} />
-                    <span>Rating</span>
-                    <input {...register("lastName", { pattern: /^[A-Za-z]+$/i })} />
-                    <input type="submit" />
+                <h2 className='fw-bold mb-4 mt-4 mb-0'>Give A Feedback</h2>
+                <form action="">
+                    <Row xs={1} md={1} lg={2} className="g-4 text-start">
+                        <Col className='mt-5'>
+                            <span className='fs-6 fw-bold'>Your Name</span>
+                            <input className='form-control w-100' value={user.displayName} type="text" />
+                        </Col>
+                        <Col>
+                            <span className='mb-0 fs-6 fw-bold'>Your Raging: {reviewCount}</span>
+                            <p>                              
+                                <ReactStars
+                                    count={5}
+                                    onChange={ratingChanged}
+                                    size={40}
+                                    isHalf={true}
+                                    emptyIcon={<i className="far fa-star"></i>}
+                                    halfIcon={<i className="fa fa-star-half-alt"></i>}
+                                    fullIcon={<i className="fa fa-star"></i>}
+                                    activeColor="#ffd700" />                
+                            </p>                               
+                        </Col>
+                    </Row>
+                    <span className='fw-bold fs-6 mt-2'>Your Address</span>
+                    <input type="text" placeholder='your address' className='form-control w-100' />
+
+                    <span className='fw-bold fs-6 mt-2'>Description</span>
+                    <textarea type="text" placeholder='your address' className='form-control w-100' />
                 </form>
             </div>
 
