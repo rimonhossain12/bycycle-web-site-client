863818c96fa2711a3e821494e3286d97d593a290
rimonspi12 <rimonmpi@gmail.com>
2022-01-06 21:25:43 +0600

    review page added

--- src/Pages/Testimonial/TestiMonialsDetails/TestiMonialsDetails.js
+++ src/Pages/Testimonial/TestiMonialsDetails/TestiMonialsDetails.js
@@ -1,46 +1,42 @@
 import React from 'react';
-import { Card } from 'react-bootstrap';
+import { Card, Col } from 'react-bootstrap';
 import testimonialImg from '../../../images/login/user.png';
 import ReactStars from "react-rating-stars-component";
+import './TestiMonialsDetails.css'
 
 
 const TestiMonialsDetails = ({ review }) => {
     const { userName, rating, address, description } = review;
 
-
     return (
-        <div className='mt-5'>
+        <Col>
             <Card>
-                <Card.Img variant="top" src={testimonialImg} style={{ borderRadius: '50%', width: '25%', marginTop: '10px' }} className='mx-auto' />
+                <Card.Img variant="top" src={testimonialImg} className='w-25 mx-auto mt-2'
+                />
                 <Card.Body>
-                    <Card.Title className='fw-bold text-warning'>{userName}</Card.Title>
-                    <p className='fw-bold text-secondary text-capitalize'>{address}</p>
-                    <div className='d-inline'>
-                        <div class="d-inline p-2 text-primary">
-                            <span className='fw-bold'>Rating: {rating}</span>
-                            <div className='mx-auto'>
-                                <ReactStars
-                                    isHalf={true}
-                                    edit={false}
-                                    size={24}
-                                    color="#666"
-                                    count={Number(rating)}
-                                    value={Number(rating)}
-                                    activeColor="#ffd700"
-                                />
-                            </div>
-                        </div>
-                        <div class="d-inline p-2 text-primary">
-                        </div>
+                    <Card.Title className='text-primary'>{userName}</Card.Title>
+                    <Card.Text className='text-center fw-bold text-info'>
+                        {address}
+                    </Card.Text>
+                    <div className='testimonial-div'>
+                        <p className='text-center'>
+                            <ReactStars
+                                classNames='user-icon'
+                                size={24}
+                                isHalf={true}
+                                edit={false}
+                                count={Number(rating)}
+                                value={Number(rating)}
+                                activeColor="#ffd700"
+                            />
+                        </p>
                     </div>
-                    <Card.Text className='text-center fw-normal'>
+                    <Card.Text className='text-center'>
                         {description}
                     </Card.Text>
                 </Card.Body>
-                <p>
-                </p>
             </Card>
-        </div>
+        </Col>
     );
 };
 
