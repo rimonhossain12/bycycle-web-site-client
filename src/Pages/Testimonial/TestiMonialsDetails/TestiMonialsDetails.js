0fc54257a5c9b3a97ab0833771884ae249fc3db6
rimonspi12 <rimonmpi@gmail.com>
2022-01-06 20:40:55 +0600

    rating added

--- src/Pages/Testimonial/TestiMonialsDetails/TestiMonialsDetails.js
+++ src/Pages/Testimonial/TestiMonialsDetails/TestiMonialsDetails.js
@@ -3,16 +3,37 @@ import { Card } from 'react-bootstrap';
 import testimonialImg from '../../../images/login/user.png';
 import ReactStars from "react-rating-stars-component";
 
-const TestiMonialsDetails = ({review}) => {
-    const {userName,rating,address,description} = review;
+
+const TestiMonialsDetails = ({ review }) => {
+    const { userName, rating, address, description } = review;
+
+
     return (
         <div className='mt-5'>
             <Card>
-                <Card.Img variant="top" src={testimonialImg} style={{borderRadius:'50%',width:'25%',marginTop:'10px'}} className='mx-auto' />
+                <Card.Img variant="top" src={testimonialImg} style={{ borderRadius: '50%', width: '25%', marginTop: '10px' }} className='mx-auto' />
                 <Card.Body>
-                    <Card.Title>{userName}</Card.Title>
-                    <p>{address}</p>
-                    <Card.Text className='text-center '>
+                    <Card.Title className='fw-bold text-warning'>{userName}</Card.Title>
+                    <p className='fw-bold text-secondary text-capitalize'>{address}</p>
+                    <div className='d-inline'>
+                        <div class="d-inline p-2 text-primary">
+                            <span className='fw-bold'>Rating: {rating}</span>
+                            <div className='mx-auto'>
+                                <ReactStars
+                                    isHalf={true}
+                                    edit={false}
+                                    size={24}
+                                    color="#666"
+                                    count={Number(rating)}
+                                    value={Number(rating)}
+                                    activeColor="#ffd700"
+                                />
+                            </div>
+                        </div>
+                        <div class="d-inline p-2 text-primary">
+                        </div>
+                    </div>
+                    <Card.Text className='text-center fw-normal'>
                         {description}
                     </Card.Text>
                 </Card.Body>
--- src/Pages/Testimonial/Testimonial/Testimonials.js
+++ src/Pages/Testimonial/Testimonial/Testimonials.js
@@ -17,7 +17,7 @@ const Testimonials = () => {
         <div className='py-5'>
             <Zoom bottom cascade>
                 <h4 className='fw-normal text-info'>What's our client says</h4>
-                <hr className='fw-normal mx-auto' style={{width:'15%',border:'0px'}} />
+                <hr className='fw-normal mx-auto' style={{ width: '15%', border: '0px', fontFamily:'Lobster'}} />
             </Zoom>
             <div className='container'>
                 <Row xs={1} md={2} lg={3} className="g-4">
